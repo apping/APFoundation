537583a1bc6704f22095247d2dd8cabac8a22ae3
Martin Öhman <martin@apping.se>
2013-07-18 15:01:13 +0200

    Added temporary class in order to build the library

---
+++ APFoundation/Temp.h
@@ -0,0 +1,21 @@
+//
+//  Temp.h
+//  APFoundation
+//
+//  Created by Martin Öhman on 2013-07-18.
+//  Copyright (c) 2013 Apping. All rights reserved.
+//
+
+/*
+ 
+    Temporär fil för att det ska gå att länka mot biblioteket
+ 
+ */
+
+
+
+#import <Foundation/Foundation.h>
+
+@interface Temp : NSObject
+
+@end
---
+++ APFoundation/Temp.m
@@ -0,0 +1,13 @@
+//
+//  Temp.m
+//  APFoundation
+//
+//  Created by Martin Öhman on 2013-07-18.
+//  Copyright (c) 2013 Apping. All rights reserved.
+//
+
+#import "Temp.h"
+
+@implementation Temp
+
+@end
