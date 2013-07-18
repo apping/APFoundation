9a8c68cbb3735119784dfe4db5afa0e7739a4cd6
Martin Öhman <martin@apping.se>
2013-07-18 13:19:35 +0200

    Added Xcode project with initial structure

---
+++ APFoundation/APFoundation.h
@@ -0,0 +1,9 @@
+//
+//  APFoundation.h
+//  APFoundation
+//
+//  Created by Martin Öhman on 2013-07-18.
+//  Copyright (c) 2013 Apping. All rights reserved.
+//
+
+
---
+++ APFoundationTests/APFoundationTests.h
@@ -0,0 +1,13 @@
+//
+//  APFoundationTests.h
+//  APFoundationTests
+//
+//  Created by Martin Öhman on 2013-07-18.
+//  Copyright (c) 2013 Apping. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface APFoundationTests : SenTestCase
+
+@end
